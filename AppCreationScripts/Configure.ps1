052574b099d954b376d0708dac1a03c1539340fa
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2023-11-20 16:49:39 +0900

    [수정] redirectUri 변경

--- AppCreationScripts/Configure.ps1
+++ AppCreationScripts/Configure.ps1
@@ -34,7 +34,7 @@ Function ConfigureApplications {
     # create the application 
     $spaAadApplication = New-MgApplication -DisplayName "ms-identity-javascript-react-spa" `
         -SignInAudience AzureADandPersonalMicrosoftAccount `
-        -Spa @{RedirectUris = "http://localhost:3001" } `
+        -Spa @{RedirectUris = "http://192.168.40.76:3001" } `
 
 
     # create the service principal of the newly created application 
